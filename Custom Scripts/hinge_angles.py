fbd87b1127ee93c207a77ed2112376a8c501c2a9
Gabriel Perko-Engel <gpe@berkeley.edu>
2020-07-08 22:01:57 -0700

    Color coded deformations.

---
+++ Custom Scripts/hinge_angles.py
@@ -0,0 +1,11 @@
+"""Provides a scripting component.
+    Inputs:
+        x: The x script variable
+        y: The y script variable
+    Output:
+        a: The a output variable"""
+
+__author__ = "gabri"
+__version__ = "2020.07.08"
+
+import rhinoscriptsyntax as rs
